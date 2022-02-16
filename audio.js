f9f39e7c7369c1bbcf70e2be808e47d11d494ea2
VISHESHK27 <61914430+visheshk27@users.noreply.github.com>
2022-02-16 11:52:48 +0530

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,17 +1,14 @@
 
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; args.callee = args.callee.caller; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
-// make it safe to use console.log always
+
 (function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
 (function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
 
 
-/*! Copyright (c) 2013 - Peter Coles (mrcoles.com)
- *  Licensed under the MIT license: http://mrcoles.com/media/mit-license.txt
- */
+
 (function() {
 
-    // test if we can use blobs
+ 
     var canBlob = false;
     if (window.webkitURL && window.Blob) {
         try {
